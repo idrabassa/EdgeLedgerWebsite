e2d0034d9cca7926f4b11897c4feef0b6e623eb9
Israel Rabassa <idrabassa@gmail.com>
2021-06-20 11:08:45 -0400

    update EdgeLedger Website

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+//Navicon Function
+function myFunction() {
+
+  var x = document.getElementById("navbar");
+  //var y=document.getElementById("navbar")
+  if (x.className === "my-navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "my-navbar";
+  }
+}
